ca42b1dc2a1d2953f9ca4502bda589181853dd14
root <root@mail.sunyque.de>
2016-06-18 15:33:02 +0200

    added directories

--- bin/clean_csv.sh
+++ bin/clean_csv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Purpose:     Clean and prepare data from bamf PDFs
-#             Replace minus signs with zeros, and delete Sum Rows
+#             Replace minus signs with zeros, and delete continent sum rows
+#             and dots in numbers 
 # 2016.06.18   S.Kim
 
 SCRIPTNAME=$(basename $0 .sh)
@@ -12,11 +13,22 @@ function usage {
 FILE=$1
 s=${FILE##*/}
 FILE_BASE=${s%.*}
+INTERMED_DIR=../intermediate
+OUTPUT_DIR=../output
+for DIR in $INTERMED_DIR $OUTPUT_DIR ; do
+  if [ ! -d $DIR ]; then
+    mkdir $DIR
+done
 # Replace minus signs
-cat $FILE | sed 's/,-/,0/g' > ${FILE_BASE}_zeros.csv 
+cat $FILE | sed 's/,-/,0/g' > $INTERMED_DIR/${FILE_BASE}_zeros.csv 
 
 # We don't want the rows with: Spalte 1, Europa, Afrika, Amerika, Asien, Unbekannt, Herkunftsläer gesamt
-cat ${FILE_BASE}_zeros.csv | sed '/^Spalte 1/d;/^Europa/d;/^Afrika/d;/^Amerika/d;/^Asien/d;/^Unbekannt/d;/^Herkunnftsl/d' > ${FILE_BASE}_cleaned.csv
+cat $INTERMED_DIR/${FILE_BASE}_zeros.csv | sed '/^Spalte 1/d;/^Europa/d;/^Afrika/d;/^Amerika/d;/^Asien/d;/^Unbekannt/d;/^Herkunnftsl/d' > $INTERMED_DIR/${FILE_BASE}_cleaned.csv
+
+if [ -f $INTERMED_DIR/${FILE_BASE}_cleaned.csv ]; then
+  mv $INTERMED_DIR/${FILE_BASE}_cleaned.csv $OUTPUT_DIR/${FILE_BASE}.csv
+fi
+# Replace dots in numbers
 
 exit 0
 
