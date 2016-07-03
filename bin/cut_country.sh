59ce7199705a4502853432b20ea9cc5b65ea4dd7
ynux <ynux@gmx.net>
2016-07-03 17:48:54 +0200

    new

---
+++ bin/cut_country.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#Purpose:      Give me a per country csv, and I'll cut columns, only leaving
+#              YearMonth, Total Applications, positive, negative, other
+# 2016.06.18   S.Kim
+
+SCRIPTNAME=$(basename $0 .sh)
+
+function usage {
+   echo "Usage: $(basename $0) file"
+}
+
+if [ $# -ne 1 ]; then
+  usage;
+  exit 2
+fi
+
+FILE=$1
+s=${FILE##*/}
+FILE_BASE=${s%.*}
+OUTPUTDIR=../cooked
+
+for DIR in $OUTPUTDIR; do
+  if [ ! -d $DIR ]; then
+    mkdir $DIR
+  fi
+done
+
+csvcut -d, -c1,5,9,10,11,12,13,14 $FILE > $OUTPUTDIR/${FILE_BASE}_cut.csv
+
+exit 0
+
