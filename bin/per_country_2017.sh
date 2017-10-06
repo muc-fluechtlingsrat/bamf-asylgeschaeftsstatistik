9fe576d294638b754a9d3fe2b54a9ace01a370f3
ynux <ynux@gmx.net>
2017-10-06 14:56:52 +0200

    get data from 2016 and 2017

--- bin/per_country_2017.sh
+++ bin/per_country_2017.sh
@@ -23,11 +23,12 @@ for DIR in $OUTPUTDIR; do
   fi
 done
 
-grep -h ${COUNTRY} $INPUTDIR/2017??.csv | dos2unix | sort > $OUTPUTDIR/${COUNTRY}_tail_2017.csv
-head -1 $INPUTDIR/header.csv | sed "s/^${FILE_FIRSTSIX}/YEAR_MONTH,/" >  $OUTPUTDIR/dateheader.csv
 
+grep -h ${COUNTRY} $INPUTDIR/2016??.csv | dos2unix | sort > $OUTPUTDIR/${COUNTRY}_tail_2017.csv
+grep -h ${COUNTRY} $INPUTDIR/2017??.csv | dos2unix | sort >> $OUTPUTDIR/${COUNTRY}_tail_2017.csv
 
 cat $OUTPUTDIR/dateheader.csv $OUTPUTDIR/${COUNTRY}_tail_2017.csv > $OUTPUTDIR/${COUNTRY}_2017.csv
+rm $OUTPUTDIR/${COUNTRY}_tail_2017.csv 
 
 exit 0
 
