9095d8fa6b6fdc229be9d1fd03862e2e8f3e5a82
ynux <ynux@gmx.net>
2017-10-16 14:03:33 +0200

    adapted script to new folder structure

--- bin/per_country.sh
+++ bin/per_country.sh
@@ -5,26 +5,30 @@
 SCRIPTNAME=$(basename $0 .sh)
 
 function usage {
-   echo "Usage: $(basename $0) country"
+   echo 'Usage: $(basename $0) -c country -y "YYYY1 YYYY2", e.g. $(basename $0) -c Syrien "2016 2017"'
 }
 
-if [ $# -ne 1 ]; then
-  usage;
-  exit 2
-fi
-
-COUNTRY=$1
-INPUTDIR=../raw
-OUTPUTDIR=../cooked
-
-for DIR in $OUTPUTDIR; do
-  if [ ! -d $DIR ]; then
-    mkdir $DIR
-  fi
+while getopts "c:y:" opt; do
+    case $opt in
+        c)  COUNTRY="$OPTARG"
+            ;;
+        y)  YEARS="$OPTARG"
+            ;;
+        *)  usage
+            ;;
+    esac
 done
 
-grep -h $COUNTRY $INPUTDIR/20????.csv | dos2unix | sort > $OUTPUTDIR/$COUNTRY_tail.csv
+INPUTDIR=../raw
+OUTPUTDIR=../cooked
+if [ ! -d $OUTPUTDIR ]; then
+  mkdir $OUTPUTDIR
+fi
+cat /dev/null > $OUTPUTDIR/$COUNTRY_tail.csv
 
+for YEAR in $YEARS; do
+  grep -h $COUNTRY $INPUTDIR/$YEAR/${YEAR}??.csv | dos2unix | sort >> $OUTPUTDIR/$COUNTRY_tail.csv
+done
 
 cat $OUTPUTDIR/dateheader.csv $OUTPUTDIR/$COUNTRY_tail.csv > $OUTPUTDIR/$COUNTRY.csv
 
