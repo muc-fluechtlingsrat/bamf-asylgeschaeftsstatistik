e3ddfffedf7bdf1c0f9314a643a1ffadeda7a274
ynux <ynux@gmx.net>
2017-10-18 17:19:53 +0200

    works now

--- bin/get_schutzquote.sh
+++ bin/get_schutzquote.sh
@@ -29,13 +29,11 @@ fi
 # Format changed slightly, we had a "" column until 201605
 # I should probably do this with csvtools ... sorry.
 sed -i 's/,"",/,/' $FILE
+# reduce to relevant columns
 csvcut -d, -c1,2,7,12,13 $FILE > $OUTPUTDIR/${FILE_BASE}_quota_tmp.csv
-awk -F, '{if ($3-$4-$5 != 0 && $3-$5 != 0){print $1,$2,$3,$4,$5,100*($3-$4-$5)/($3-$5), 100*($3-$4-$5)/($3)} else {print $1,$2,$3,$4,"n.d.,n.d"}}' $OUTPUTDIR/${FILE_BASE}_quota_tmp.csv > $OUTPUTDIR/${FILE_BASE}_quota.csv
-# cannot cope with 2016,"Kongo, Dem. Republik",168,96,43
-
-sed '1s/.*/YEAR_MONTH,Asylberechtigt,Fluechtling, subs. Schutz,Abschiebungsverbot,Abgelehnt,sonstige Verfahrenserledigungen/' $OUTPUTDIR/${FILE_BASE}_quota.csv > $OUTPUTDIR/tmpfile.csv
-
-mv $OUTPUTDIR/tmpfile.csv  $OUTPUTDIR/${FILE_BASE}_quota.csv
-
+# vFPAT to handle comma in quotes, like "Kongo, Dem. Republik"
+awk -vFPAT='([^,]*)|("[^"]+")' -vOFS=, '{if ($4-$4-$5 != 0 && $3-$5 != 0){print $1,$2,$3,$4,$5,100*($3-$4-$5)/($3-$5), 100*($3-$4-$5)/($3)} else {print $1,$2,$3,$4,$5,"nd,nd"}}' $OUTPUTDIR/${FILE_BASE}_quota_tmp.csv > $OUTPUTDIR/${FILE_BASE}_quota.csv
+# add header
+sed -i '1s/.*/YEAR_MONTH,Herkunftsland, Entscheidungen insgesamt,Ablehnungen,sonstige Erledigungen, bereinigte Schutzquote, BAMF-Schutzquote/' $OUTPUTDIR/${FILE_BASE}_quota.csv 
 exit 0
 
