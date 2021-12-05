cf0052ec9f8fffd2da6118ea1063384d731329ed
ynux <ynux@gmx.net>
2021-12-05 20:51:03 +0100

    start from root, and cd into git repo

--- bin/pdf_to_raw.sh
+++ bin/pdf_to_raw.sh
@@ -22,6 +22,8 @@ while getopts 'ymt:h' opt; do
 done
 shift "$(($OPTIND -1))"
 
+# written for a docker container, everything is in /
+
 # translate to German month name. You need the locale generated for this.
 MONATSNAME=$(LC_ALL=de_DE.utf8 date -d "2000-$MONTH-11" +%B)
 PDF=hkl-antrags-entscheidungs-bestandsstatistik-$MONATSNAME_LC-$YEAR.pdf
@@ -30,11 +32,11 @@ PDF=hkl-antrags-entscheidungs-bestandsstatistik-$MONATSNAME_LC-$YEAR.pdf
 wget "https://www.bamf.de/SharedDocs/Anlagen/DE/Statistik/Asylgeschaeftsstatistik/$PDF?__blob=publicationFile&v=2" -O $PDF
 
 # run tabula.sh 
-./bamf-asylgeschaeftsstatistik/bin/tabula.sh $PDF $YEAR$MONTH.csv
+/bamf-asylgeschaeftsstatistik/bin/tabula.sh $PDF $YEAR$MONTH.csv
 
 # git push the new csv
-mv $YEAR$MONTH.csv bamf-asylgeschaeftsstatistik/raw/$YEAR
-git add ../raw/$YEAR/$YEAR$MONTH*.csv
-git -c "user.name=ynux" -c "user.email=ynux@gmx.net" commit -m "adding raw $MONATSNAME $YEAR data" ../raw/$YEAR/$YEAR$MONTH*.csv
-
+mv $YEAR$MONTH.csv /bamf-asylgeschaeftsstatistik/raw/$YEAR
+cd /bamf-asylgeschaeftsstatistik 
+git add raw/$YEAR/$YEAR$MONTH*.csv
+git -c "user.name=ynux" -c "user.email=ynux@gmx.net" commit -m "adding raw $MONATSNAME $YEAR data" raw/$YEAR/$YEAR$MONTH*.csv
 git push https://ynux:$GITTOKEN@github.com/muc-fluechtlingsrat/bamf-asylgeschaeftsstatistik.git
