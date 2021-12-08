7dbdf295afbfacef3c50f7a0c58e6ed46a8fd941
ynux <ynux@gmx.net>
2021-12-08 16:07:25 +0100

    add -s flag to have sonstiges, or not

--- bin/cut_country.sh
+++ bin/cut_country.sh
@@ -1,59 +1,41 @@
 #!/bin/bash
-#Purpose:      Give me a per country csv, and I'll cut columns, only leaving
+# Purpose:     Give me a per country csv, and I'll cut columns, only leaving
 #              YearMonth, Total Applications, positive, negative, other
-#              If d "delete to" is set, delete old months up to this date
 # 2016.06.18   S.Kim
+# 2021.12.08   remove option to cut by date
 
-set -euo pipefail
-#set -vx
+set -vx
 
 SCRIPTNAME=$(basename $0 .sh)
 
 function usage {
-   echo 'Usage: $(basename $0) -f file_cooked_country.csv -d YYYY-MM, e.g. ./cut_country.sh -f ../cooked/Iran.csv -d "2016-12" '
+   echo 'Usage: $(basename $0) -f <file_cooked_country>.csv -o <outputdir> -s'
 }
 
-while getopts "f:d:" opt; do
+while getopts "f:o:s" opt; do
     case $opt in
         f)  FILE="$OPTARG"
             ;;
-        d)  SINCE="$OPTARG"
+        o)  OUTPUTDIR="$OPTARG"
             ;;
-        *)  usage
+        s)  SONSTIGE=true
+            ;;
+        *)  usage; exit 0
             ;;
     esac
 done
 
-SINCE="${SINCE:-FULL}"
 s=${FILE##*/}
 FILE_BASE=${s%.*}
-OUTPUTDIR=../cooked/
 
 if [ ! -d $OUTPUTDIR ]; then
   mkdir $OUTPUTDIR
 fi
-# only use since if you find it
-set +e
-SINCE_IN_DATES=$(grep -c $SINCE $FILE)
-if [ $SINCE_IN_DATES -eq 0 ]; then
-  echo "Couldn't find the date $SINCE in your data, assuming FULL"
-  SINCE="FULL"
+
+if [[ $SONSTIGE ]]; then 
+csvcut -d, -c "YEAR_MONTH,ENTSCHEIDUNGEN ueber Asylantraege Anerkennungen als Asylberechtigte (Art 16a und Familienasyl),ENTSCHEIDUNGEN ueber Asylantraege Anerkennungen als Fluechtling gem. Par. 3 I AsylG,ENTSCHEIDUNGEN ueber Asylantraege Gewaehrung von subsidiaerem Schutz gem. Par. 4 I AsylG,ENTSCHEIDUNGEN ueber Asylantraege Feststellung eines Abschiebungsverbotes gem. Par. 60 V/VII AufenthG,ENTSCHEIDUNGEN ueber Asylantraege Ablehnungen (unbegr. abgel./offens unbegr. abgel.),ENTSCHEIDUNGEN ueber Asylantraege sonstige Verfahrenserledigungen" $FILE > $OUTPUTDIR/${FILE_BASE}_cut.csv
+sed -i.bak '1s/.*/YEAR_MONTH,Asylberechtigt,Fluechtling,subs. Schutz,Abschiebungsverbot,Abgelehnt,sonstige Verfahrenserledigungen/' $OUTPUTDIR/${FILE_BASE}_cut.csv
+else
+csvcut -d, -c "YEAR_MONTH,ENTSCHEIDUNGEN ueber Asylantraege Anerkennungen als Asylberechtigte (Art 16a und Familienasyl),ENTSCHEIDUNGEN ueber Asylantraege Anerkennungen als Fluechtling gem. Par. 3 I AsylG,ENTSCHEIDUNGEN ueber Asylantraege Gewaehrung von subsidiaerem Schutz gem. Par. 4 I AsylG,ENTSCHEIDUNGEN ueber Asylantraege Feststellung eines Abschiebungsverbotes gem. Par. 60 V/VII AufenthG,ENTSCHEIDUNGEN ueber Asylantraege Ablehnungen (unbegr. abgel./offens unbegr. abgel.)" $FILE > $OUTPUTDIR/${FILE_BASE}_cut_nixsonst.csv
+sed -i.bak '1s/.*/YEAR_MONTH,Asylberechtigt,Fluechtling,subs. Schutz,Abschiebungsverbot,Abgelehnt/' $OUTPUTDIR/${FILE_BASE}_cut_nixsonst.csv
 fi
-set -e
-
-# Format changed slightly, we had a "" column until 201605
-# I should probably do this with csvtools ... sorry.
-sed -i.bak 's/,"",/,/' $FILE
-rm $FILE.bak
-csvcut -d, -c1,8,9,10,11,12,13 $FILE > $OUTPUTDIR/${FILE_BASE}_cut.csv
-sed '1s/.*/YEAR_MONTH,Asylberechtigt,Fluechtling, subs. Schutz,Abschiebungsverbot,Abgelehnt,sonstige Verfahrenserledigungen/' $OUTPUTDIR/${FILE_BASE}_cut.csv > $OUTPUTDIR/tmpfile.csv
-if [ "$SINCE" = "FULL" ]; then
-  mv $OUTPUTDIR/tmpfile.csv  $OUTPUTDIR/${FILE_BASE}_cut.csv
-else 
- # echo "Removing rows before $SINCE"
- sed "2,/^$SINCE/d" $OUTPUTDIR/tmpfile.csv > $OUTPUTDIR/${FILE_BASE}_cut.csv
-fi
-
-echo "$OUTPUTDIR/${FILE_BASE}_cut.csv"
-exit 0
-
